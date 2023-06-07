ccbfa440a6d7aa146b411376ab3973b2e03d8c4b
codelahiru <gayan.hatharasinghe@gmail.com>
2023-06-07 15:46:43 +0530

    continuation from modules

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("Hello from Node.js!");
+//console.log("Hello from Node.js!");
+
+const Person = require('./person');
+
+// Instantiate
+
+const person1 = new Person('John Doe');
+
+
 
---
+++ person.js
@@ -0,0 +1,30 @@
+// const person = {
+//     name: 'John Doe',
+//     age: 30,
+
+// }
+
+// module.exports = person;
+
+// const obj = {
+//     name: 'John',
+//     greet: function() {
+//       console.log(`Hello, ${this.name}!`);
+//     }
+//   };
+  
+//   obj.greet(); // Output: Hello, John!
+  
+class Person {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+greeting(){
+    console.log(`My name is ${this.name} and I am ${this.age} years old`)
+}
+
+module.exports = Person;
+
