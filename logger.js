18cc474af33728c4f382989d1c7340ab64a42bec
codelahiru <gayan.hatharasinghe@gmail.com>
2023-07-18 23:44:53 +0530

    071823-23.44

--- logger.js
+++ logger.js
@@ -12,3 +12,21 @@ class Logger extends EventEmitter{
 }
 
 module.exports = Logger;
+
+// //console.log("Hello from Node.js!");
+
+// const Person = require('./person');
+
+// // Instantiate
+
+// const person1 = new Person('John Doe', 30);
+
+// person1.greeting();
+
+// const Logger = require('./logger');
+
+// const logger = new Logger();    // Instantiation
+
+// logger.on('message', data => console.log('Called listener ', data));
+
+// logger.log('Hello World');
