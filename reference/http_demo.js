cb392896b9ba505f725659b5ee470b39f96505f2
codelahiru <gayan.hatharasinghe@gmail.com>
2023-07-27 22:56:36 +0530

    072723-22.56

--- reference/http_demo.js
+++ reference/http_demo.js
@@ -1,5 +1,12 @@
 const http = require('node:http');
 
+/*Source Code: lib/http.js
+To use the HTTP server and client one must require('node:http').
+The HTTP interfaces in Node.js are designed to support many features of the protocol which have been traditionally difficult to use. 
+In particular, large, possibly chunk-encoded, messages. The interface is careful to never buffer entire requests or responses, 
+so the user is able to stream data. */
+
+
 // Create server object
 http.createServer((req, res) => {
     //write response
