9df0b37dfcbcec796fb6dee94c9092c8fbb55cda
codelahiru <gayan.hatharasinghe@gmail.com>
2024-02-27 16:22:51 +0530

    Node Core Modules - path, file system, os

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -10,4 +10,11 @@ console.log(path.dirname(__filename));
 console.log(path.extname(__filename));
 
 // create path object
-console.log(path.parse(__filename).base);
+console.log(path.parse(__filename));
+
+// create path object
+console.log(path.parse(__filename).root);
+
+// concatenate paths
+
+console.log(path.join(__dirname, 'test', 'hello.html'))
