be5c38790b5f288ee65fb63fe62f4ed68e682dc3
codelahiru <gayan.hatharasinghe@gmail.com>
2024-02-27 23:01:59 +0530

    server demo

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-// const http = require('http');
-// const path = require('path');
-// const fs = require('fs');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 
-// const server =  http.createServer((req, res) => {
-//     console.log(req.url);
-// });
+const server =  http.createServer((req, res) => {
+    console.log(req.url);
+});
 
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
-// server.listen(PORT, () => {
-//     console.log(`Server running on ${PORT}`);
-// });
+server.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
 
-const Person = require('./person');
+// const Person = require('./person');
 
-// extantiate
-const person1 = new Person('John Doe', 31);
+// // extantiate
+// const person1 = new Person('John Doe', 31);
 
-person1.greeting();
+// person1.greeting();
 
